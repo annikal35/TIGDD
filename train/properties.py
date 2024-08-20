c8d3f8855d7e3dd2ce1ddb41479e0a8a8a82323f
annikal35 <103857252+annikal35@users.noreply.github.com>
2024-08-20 19:15:31 +0900

    Update properties.py

--- train/properties.py
+++ train/properties.py
@@ -1,28 +1,3 @@
-class RabbitConnection:
-    hostName = "127.0.0.1"
-    queueName = "graph_stream"
-    routingKey = "graph_stream"
-    heartbeat = 0
-
-class SubGen:
-    s1_positive_sample = "subgen/pos"
-    s1_negative_sample = "subgen/neg"
-    s2_positive_sample = "subgen/s2pos"
-    s2_negative_sample = "subgen/s2neg"
-    run_command = "subgen/subgen"
-
-class GBAD:
-    gbad_home = "gbad"
-    graph_folder = "gbad/graphs"
-    subgraph = "subgraph.g"
-    graph_file = "G.g"
-    positive_graph = "pos_G.g"
-    negative_graph = "neg_G.g"
-    minsize = 1
-    maxsize = 10
-    run_command = "bin/gbad " + " -minsize " + str(minsize) + " -maxsize " + str(maxsize)
-    algorithm = "mdl"
-
 class SubDue:
     SubDueFolder ="subdue"
     graphFolder = "subdue/graphs"
@@ -50,14 +25,3 @@ class RULSIF:
     k_fold = 5
     th = 3.4
 
-class DataList:
-    # datasetname = (<dataset name>, <total graph>, <expected drift points>, <graph file name>, <isSyntetic (True or False)> , <param>(parameter array))
-    SD1 = ('SD1', 2000, [1000], 'dataset/SD1.g', True, Experiment.param)
-    SD2 = ('SD2', 6000, [1001, 2001, 3001, 4001, 5001], 'dataset/SD2.g', False, Experiment.param)
-    DBLP = ('DBLP', 2000, [1000], 'dataset/DBLP.g', False, Experiment.param)
-    AIDS = ('AIDS', 2000, [1600], 'dataset/aids.g', False, Experiment.param)
-    DOS = ('DOS', 3201, [2980], 'dataset/DOS.g', False, Experiment.param)
-    MUTA = ('MUTA', 4337, [2402], 'dataset/muta.g', False, Experiment.param)
-
-    #make a list of all datasets
-    data_list = [DBLP]
